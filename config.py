de26b925e3b0605a0cc663d698be655321b18d89
Ali-RT <ali.rahim.taleqani@gmail.com>
2023-07-27 08:09:08 -0700

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+PARAMS = {
+    "emb_size": 100,
+    "epochs": 10,
+    "batch_size": 256,
+    "learning_rate": 0.01,
+    "model_name": "MF",  # MF or CollabFNet
+    "loss_fn": "MSELoss",
+    "optimizer": "Adam",
+    "test_size": 0.2,
+}
